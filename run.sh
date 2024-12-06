9d5709062681e6d01a289e8f16a6442cbbc66c1b
Raghuram Subramani <raghus2247@gmail.com>
2024-12-06 19:26:32 +0530

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 [directory]"
+  exit
+fi
+
 user=compromyse
 
-docker run -it --rm -v "$(pwd)/src:/home/$user/src" -v "$HOME/.ssh:/home/$user/.ssh" lineage
+docker run -it --rm -v "$(pwd)/$1:/home/$user/src" -v "$HOME/.ssh:/home/$user/.ssh" lineage
