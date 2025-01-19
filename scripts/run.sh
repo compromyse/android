ff32d10d3d53bba4dc737119fad0a51ac9fe372e
Raghuram Subramani <raghus2247@gmail.com>
2025-01-19 16:50:21 +0530

    docker->podman

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,5 @@ fi
 user=compromyse
 
 pushd $1
-docker run -it --rm -v "$(pwd):/home/$user/src" -v "$HOME/.ssh:/home/$user/.ssh" lineage
+podman run -it --rm -v "$(pwd):/home/$user/src" -v "$HOME/.ssh:/home/$user/.ssh" lineage
 popd
